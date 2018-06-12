1e2be38180cffa19f5fed5b801cd2b9e43a5fc51
Diogo Leite <diogo.development@gmail.com>
2018-06-12 14:51:35 -0300

    Commit inicial, criado um novo projeto javaFX

---
+++ src/br/com/diogoleite/sorteador/Main.java
@@ -0,0 +1,17 @@
+package br.com.diogoleite.sorteador;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		
+		
+		stage.setTitle("Sorteador");
+		stage.show();
+	}
+	
+	public static void main(String args[]) {
+		launch();
+	}
+}
